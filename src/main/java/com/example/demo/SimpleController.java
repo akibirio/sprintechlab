033c69be19f7b9a3804cb1c33a67f5c41006f3fe
AKIBIRIO <ayubkibirio@gmail.com>
2021-04-30 13:44:31 +0300

    Added Secong COntroller Method, that prints name

--- src/main/java/com/example/demo/SimpleController.java
+++ src/main/java/com/example/demo/SimpleController.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 @RestController
 public class SimpleController {
 
@@ -11,5 +13,13 @@ public class SimpleController {
 
         return "Hello Ayub! Congratulations you have connected your GitHub account with VSCode Via Git";
     }
+
+    @RequestMapping("/hello")
+    public String printFullName(){
+        return "Ayub Kibirio!";
+    }
+
+
+    
     
 }
